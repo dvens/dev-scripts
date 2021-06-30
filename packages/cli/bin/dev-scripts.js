b9bc91f6b1e001c4d49bfabf6900ca87b3e952e3
dvens <d.joelvens@gmail.com>
2021-06-30 10:40:05 +0200

    added start script

--- packages/cli/bin/dev-scripts.js
+++ packages/cli/bin/dev-scripts.js
@@ -2,8 +2,40 @@
 const { program } = require('commander');
 
 const packageJson = require('../package.json');
+const semver = require('semver');
 
 program.version(packageJson.version);
 
+const requiredVersion = packageJson.engines.node;
+
+function checkNodeVersion(wanted, id) {
+    if (!semver.satisfies(process.version, wanted, { includePrerelease: true })) {
+        console.log(
+            chalk.red(
+                'You are using Node ' +
+                    process.version +
+                    ', but this version of ' +
+                    id +
+                    ' requires Node ' +
+                    wanted +
+                    '.\nPlease upgrade your Node version.',
+            ),
+        );
+        process.exit(1);
+    }
+}
+
+checkNodeVersion(requiredVersion, '@dev-scripts/cli');
+
+program
+    .command('start')
+    .description('alias of "npm run start" in the current project')
+    .allowUnknownOption()
+    .action(() => {
+        process.env.NODE_ENV = 'production';
+        require('../dist/start')();
+    });
+
+//https://github.com/vuejs/vue-cli/blob/dev/packages/%40vue/cli/bin/vue.js
 
 program.parse(process.argv);
