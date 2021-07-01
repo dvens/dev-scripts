96b0b84f979dd86d03972ebc8f4aed7fb7ab969d
dvens <d.joelvens@gmail.com>
2021-07-01 11:41:22 +0200

    changed webpack versioning

--- packages/cli/src/start.ts
+++ packages/cli/src/start.ts
@@ -7,10 +7,9 @@ import {
 import { create } from 'browser-sync';
 import * as express from 'express';
 import { resolve } from 'path';
-
-import webpack = require('webpack');
-import webpackDevMiddleware = require('webpack-dev-middleware');
-import webpackHotMiddleware = require('webpack-hot-middleware');
+import * as webpack from 'webpack';
+import * as webpackDevMiddleware from 'webpack-dev-middleware';
+import * as webpackHotMiddleware from 'webpack-hot-middleware';
 
 const browserSyncServer = create();
 const server = express();
